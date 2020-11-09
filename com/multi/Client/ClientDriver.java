639a7b77085c9403adeb086f5e2f351e8b586b03
Parker Brandt <parker.brandt101@gmail.com>
2020-11-09 17:58:46 -0600

    Implemented basic outline

--- com/multi/Client/ClientDriver.java
+++ com/multi/Client/ClientDriver.java
@@ -1,12 +1,57 @@
 package com.multi.Client;
 
+// Imports
+import javax.swing.*;
+
+
 /**
  * The main class for the Client machine
+ * Will essentially host 2 GUIs - Login and Main
  *
  * @author Lucentus
  * @version Nov 9th, 2020
  */
-public class ClientDriver
+public class ClientDriver extends JFrame
 {
+    // Properties
 
+    // Swing Properties
+
+
+    // Constructors
+    /**
+     * Default Constructor
+     */
+    public ClientDriver()
+    {
+
+    }
+
+
+    // Methods
+    /**
+     * Main Method of Client Machine
+     * Start of program logic
+     * @param args is unused at this time (subject to change)
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+    /**
+     *
+     */
+    private void createAndRunLoginGUI()
+    {
+
+    }
+
+    /**
+     * 
+     */
+    private void createAndRunHomeGUI()
+    {
+
+    }
 }
