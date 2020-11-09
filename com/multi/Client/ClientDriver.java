74ec4ca7b40f79765e21fcb746edad6bba57d2a1
Parker Brandt <parker.brandt101@gmail.com>
2020-11-09 17:52:43 -0600

    Initial commit

---
+++ com/multi/Client/ClientDriver.java
@@ -0,0 +1,12 @@
+package com.multi.Client;
+
+/**
+ * The main class for the Client machine
+ *
+ * @author Lucentus
+ * @version Nov 9th, 2020
+ */
+public class ClientDriver
+{
+
+}
---
+++ com/multi/Server/ServerDriver.java
@@ -0,0 +1,12 @@
+package com.multi.Server;
+
+/**
+ * The main class for the Server machine
+ *
+ * @author Lucentus
+ * @version Nov 9th, 2020
+ */
+public class ServerDriver
+{
+
+}
